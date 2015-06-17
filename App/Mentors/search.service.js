a3219901a11f791574edd4bb9e7cc43c3b82e1f7
Matthew Ellwood <Seaal@users.noreply.github.com>
2015-06-17 23:51:24 +0100

    Fixed champ images for the search

--- App/Mentors/search.service.js
+++ App/Mentors/search.service.js
@@ -19,8 +19,8 @@
                 profilePictureAccount: { name: 'Seaal', region: 'NA'},
                 rating: 1.5,
                 lastActive: '2015-05-13 21:28:00',
-                champions: [{ name: 'Lux', imageUrl: 'images/lux-32.jpg' },
-                    { name: 'Shaco', imageUrl: 'images/shaco-32.jpg' }, { name: 'Ezreal', imageUrl: 'images/ezreal-32.jpg' }, { name: 'Swain', imageUrl: 'images/swain-32.jpg' }, { name: 'Draven', imageUrl: 'images/draven-32.jpg' }, { name: 'Janna', imageUrl: 'images/janna-32.jpg' }, { name: 'Lee Sin', imageUrl: 'images/leesin-32.jpg' }],
+                champions: [{ name: 'Lux', imageUrl: 'images/lux-48.jpg' },
+                    { name: 'Shaco', imageUrl: 'images/shaco-48.jpg' }, { name: 'Ezreal', imageUrl: 'images/ezreal-48.jpg' }, { name: 'Swain', imageUrl: 'images/swain-48.jpg' }, { name: 'Draven', imageUrl: 'images/draven-48.jpg' }, { name: 'Janna', imageUrl: 'images/janna-48.jpg' }, { name: 'Lee Sin', imageUrl: 'images/leesin-48.jpg' }],
                 badges: [{ name: 'Experienced Mentor', extraInfo: 'Has completed 30 mentor sessions.' }, { name: 'Instructor', extraInfo: 'Helps out in Mentored Inhouses.' }, { name: 'Admin', extraInfo: 'Helps run Elo Heaven.' }]
             }, {
                 name: 'Qichin',
@@ -29,8 +29,8 @@
                 profilePictureAccount: { name: 'Qichin', region: 'NA'},
                 rating: 5,
                 lastActive: '2010-05-06 09:32:00',
-                champions: [{ name: 'Lux', imageUrl: 'images/lux-32.jpg' },
-                    { name: 'Shaco', imageUrl: 'images/shaco-32.jpg' }, { name: 'Ezreal', imageUrl: 'images/ezreal-32.jpg' }, { name: 'Swain', imageUrl: 'images/swain-32.jpg' }, { name: 'Draven', imageUrl: 'images/draven-32.jpg' }, { name: 'Janna', imageUrl: 'images/janna-32.jpg' }, { name: 'Lee Sin', imageUrl: 'images/leesin-32.jpg' }],
+                champions: [{ name: 'Lux', imageUrl: 'images/lux-48.jpg' },
+                    { name: 'Shaco', imageUrl: 'images/shaco-48.jpg' }, { name: 'Ezreal', imageUrl: 'images/ezreal-48.jpg' }, { name: 'Swain', imageUrl: 'images/swain-48.jpg' }, { name: 'Draven', imageUrl: 'images/draven-48.jpg' }, { name: 'Janna', imageUrl: 'images/janna-48.jpg' }, { name: 'Lee Sin', imageUrl: 'images/leesin-48.jpg' }],
                 badges: [{ name: 'Experienced Mentor', extraInfo: 'Has completed 30 mentor sessions.' }, { name: 'Admin', extraInfo: 'Helps run Elo Heaven.' }]
             }];
 
