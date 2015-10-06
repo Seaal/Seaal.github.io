4192301f9812babce6f520015754aa6cf4d5249a
Seaal <Seaal@users.noreply.github.com>
2015-10-06 09:19:28 +0100

    Can now add multiple specialities, delete button also added to specialities

--- App/Mentors/Application/bio/bio.controller.js
+++ App/Mentors/Application/bio/bio.controller.js
@@ -7,10 +7,15 @@
 	function BioController() {
 		var vm = this;
 		
-		vm.specialities = [];
+		vm.specialities = [{ text: "" }];
 		vm.summary = "";
 		vm.aboutMentor = "";
 		vm.maxSummaryLength = 500;
+		vm.addSpeciality = addSpeciality;
+		
+		function addSpeciality() {
+			vm.specialities.push({ text: "" });
+		}
 	}
 	
 })();
